a6f3a690dd9d35b9336231a9fb947e9231737c8a
Petros Chatzipetrou <petro.chatzipetrou@gmail.com>
2025-08-19 21:34:26 +0200

    Squashed commit of the following:
    commit f5358ee2b05462de3f9d247e023ebb1a5551cb24
    Author: Petros Chatzipetrou <petro.chatzipetrou@gmail.com>
    Date:   Tue Aug 19 21:31:17 2025 +0200
    
        Create cart context and use it throughtout code
    
    commit ed84b3fb5ae3432417cc137ab0e883c694709586
    Author: Petros Chatzipetrou <petro.chatzipetrou@gmail.com>
    Date:   Thu Aug 14 21:16:51 2025 +0200
    
        Implement login logout logic with userContext

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,25 @@
 import { Link, Outlet } from "react-router-dom";
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { ReactComponent as AppLogo } from "../../assets/crown.svg";
 import "./navigation.styles.scss";
+import { UserContext } from "../../contexts/user.context";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.components";
+import { CartContext } from "../../contexts/cart.context";
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
+
+  const signOutHandler = async () => {
+    try {
+      await signOutUser();
+    } catch (error) {
+      console.log(error);
+      alert("There was an error signing out");
+    }
+  };
   return (
     <Fragment>
       <div className="navigation">
@@ -14,10 +30,18 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className="nav-link" to="/sign-in">
-            Sign in
-          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutHandler}>
+              Sign out
+            </span>
+          ) : (
+            <Link className="nav-link" to="/sign-in">
+              Sign in
+            </Link>
+          )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
